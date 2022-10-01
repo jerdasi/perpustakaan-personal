309ab95073ed36c3279efaa00c7b8610921274d4
Jeremia Daniel Silitonga <48269741+jerdasi@users.noreply.github.com>
2022-10-01 14:38:39 +0700

    First Milestone Create Layout For The Web

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+// window.addEventListener("load", () => {
+//     console.log("load");
+// })
+
+console.log("selesai");
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("DOMContentLoaded")
+})
+
+document.addEventListener("unload", () => {
+    console.log("unload");
+})
