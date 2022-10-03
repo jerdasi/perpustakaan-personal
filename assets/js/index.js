fe872577bfd8053e4861ddca9a3fd6155a77ea5b
jerdasi <jeremiadansil@gmail.com>
2022-10-03 07:30:45 +0700

    Update tak seberapa

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,29 @@
-// window.addEventListener("load", () => {
-//     console.log("load");
-// })
-
-console.log("selesai");
-
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("DOMContentLoaded")
-})
+  const RENDER_EVENT = "render-buku";
+  const STORAGE_KEY = "ALL_BOOKS";
 
-document.addEventListener("unload", () => {
-    console.log("unload");
-})
+  const inputNama = document.getElementById("input-nama-buku");
+  const inputJudul = document.getElementById("input-judul-buku");
+  const inputPenulis = document.getElementById("input-penulis-buku");
+  const inputTahun = document.getElementById("input-terbit-buku");
+  const buttonSimpan = document.getElementById("button-simpan-buku");
+
+  const submitForm = document.querySelector("form");
+
+  submitForm.addEventListener("submit", (ev) => {
+    ev.preventDefault();
+    let book = {
+      id: +new Date(),
+      title: inputJudul.value,
+      author: inputPenulis.value,
+      inputTahun: inputTahun.value,
+    };
+    console.log(book);
+  });
+
+  const tambahBuku = () => {};
+
+  const editBuku = () => {};
+
+  const hapusBuku = () => {};
+});
