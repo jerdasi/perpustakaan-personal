85523bd337dc931eedbe97ccdfe4f21ffb51717a
jerdasi <jeremiadansil@gmail.com>
2022-10-10 06:27:55 +0700

    Otw Submit, Fixing Logic of CRUD

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,6 +6,7 @@ const TITLE_MODAL_EDIT = "Edit Buku";
 
 // Data
 let data = [];
+let selectedOnEdit = -1;
 
 // Filter
 const filterInputNama = document.getElementById("nama-buku");
@@ -24,6 +25,7 @@ const inputPenulis = document.getElementById("input-penulis-buku");
 const inputTahun = document.getElementById("input-terbit-buku");
 const inputStatusBaca = document.getElementById("input-selesai-buku");
 const buttonSimpan = document.getElementById("button-simpan-buku");
+const buttonEdit = document.getElementById("button-edit-buku");
 
 // Notification
 const notification = document.querySelector(".notification-container");
@@ -32,9 +34,7 @@ const closeNotification = document.querySelector(".close-notification");
 // Content Container
 const mainContainer = document.querySelector(".content-wrapper");
 
-// Form
-const submitForm = document.querySelector("form");
-
+// Function for Checking Compatibel of Storage
 const isStorageExist = () => {
   if (typeof Storage === undefined) {
     showNotification({
@@ -46,6 +46,15 @@ const isStorageExist = () => {
   return true;
 };
 
+// Checking Field Form Value
+const checkingFormValue = () => {
+  if (inputJudul.value && inputPenulis.value && inputTahun.value) {
+    return true;
+  }
+  return false;
+};
+
+// Function for showNotificaton for 5s
 const showNotification = ({ headerMsg = "", bodyMsg = "" }) => {
   notification.querySelector(".notification-container-header h1").innerText =
     headerMsg;
@@ -57,11 +66,15 @@ const showNotification = ({ headerMsg = "", bodyMsg = "" }) => {
   }, 5000);
 };
 
+// Event for hiddenNotification by Click
 closeNotification.addEventListener("click", () => {
+  selectedOnEdit = -1;
   notification.classList.add("hidden");
 });
 
+// Function for Show Modal For Add Book
 const toggleModalTambahBuku = () => {
+  clearModalTambahBuku();
   const classTambah = modalTambahBuku.classList.contains("hidden")
     ? ""
     : "hidden";
@@ -70,37 +83,48 @@ const toggleModalTambahBuku = () => {
     : modalTambahBuku.classList.remove("hidden");
 };
 
+// Function for clear Field in Modal
 const clearModalTambahBuku = () => {
+  selectedOnEdit = -1;
   inputJudul.value = inputPenulis.value = "";
   inputTahun.value = 2022;
   inputStatusBaca.checked = false;
 };
 
+// Function for clear filter
 const clearFilter = () => {
   filterInputNama.value = "";
   filterSelectTipe.value = 0;
 };
 
+// Function to Filter Book by Name or By Status
 const filterBuku = () => {
-  if (filterInputNama.value) {
-    if (filterSelectTipe.value != 0) {
-      let status = filterSelectTipe.value == 1 ? false : true;
-      data = data.filter(
-        (item) =>
-          item.isComplete == status &&
-          item.title.toLowerCase().includes(filterInputNama.value.toLowerCase())
-      );
-    } else {
+  getDataFromStorage();
+
+  let status = filterSelectTipe.value == 1 ? false : true;
+
+  if (filterInputNama.value && filterSelectTipe.value != 0) {
+    data = data.filter(
+      (item) =>
+        item.isComplete == status &&
+        item.title.toLowerCase().includes(filterInputNama.value.toLowerCase())
+    );
+  } else {
+    if (filterInputNama.value) {
       data = data.filter((item) =>
         item.title.toLowerCase().includes(filterInputNama.value.toLowerCase())
       );
+    } else if (filterSelectTipe.value != 0) {
+      data = data.filter((item) => item.isComplete == status);
+    } else {
+      data = [...data];
     }
-  } else {
-    data = [...data];
   }
+
   document.dispatchEvent(new Event(RENDER_EVENT));
 };
 
+// Function to Save in LocalStorage
 const saveDataToStorage = () => {
   if (isStorageExist()) {
     localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
@@ -108,6 +132,7 @@ const saveDataToStorage = () => {
   document.dispatchEvent(new Event(RENDER_EVENT));
 };
 
+// Function to Get Data From Local Storage
 const getDataFromStorage = () => {
   if (isStorageExist()) {
     let result = localStorage.getItem(STORAGE_KEY);
@@ -118,55 +143,86 @@ const getDataFromStorage = () => {
   }
 };
 
+// Function for show Edit Book and Show Modal to Edit
 const tampilkanBuku = (indexBuku) => {
   modalHeader.innerText = TITLE_MODAL_EDIT;
-  const book = data[indexBuku];
-  inputJudul.value = book.title;
-  inputPenulis.value = book.author;
-  inputTahun.value = book.year;
-  inputStatusBaca.checked = book.isComplete;
-  submitForm.addEventListener("submit", (ev) => {
-    ev.preventDefault();
-    editBuku(indexBuku);
+  buttonSimpan.classList.add("hidden");
+  buttonEdit.classList.remove("hidden");
 
-    setTimeout(() => {
-      showNotification({
-        headerMsg: "Berhasil",
-        bodyMsg: "Berhasil Mengedit Buku!",
-      });
-    }, 100);
-  });
+  selectedOnEdit = indexBuku;
+
   toggleModalTambahBuku();
+  const { title, author, year, isComplete } = data[indexBuku];
+  inputJudul.value = title;
+  inputPenulis.value = author;
+  inputTahun.value = year;
+  inputStatusBaca.checked = isComplete;
 };
 
+// Function for add Book to Data
 const tambahBuku = () => {
-  let book = {
-    id: +new Date(),
-    title: inputJudul.value,
-    author: inputPenulis.value,
-    year: inputTahun.value,
-    isComplete: inputStatusBaca.checked,
-  };
-  data.push(book);
-  saveDataToStorage();
-
-  clearModalTambahBuku();
-  toggleModalTambahBuku();
+  try {
+    let book = {
+      id: +new Date(),
+      title: inputJudul.value,
+      author: inputPenulis.value,
+      year: inputTahun.value,
+      isComplete: inputStatusBaca.checked,
+    };
+    data.push(book);
+    saveDataToStorage();
+    toggleModalTambahBuku();
+    showNotification({
+      headerMsg: "Berhasil Menambah!",
+      bodyMsg: "Berhasil Menambah Informasi Buku",
+    });
+  } catch (err) {
+    showNotification({
+      headerMsg: "Gagal Menambah!",
+      bodyMsg: err.message,
+    });
+  }
 };
 
-const editBuku = (indexBuku) => {
-  console.log(data[indexBuku]);
+const editBuku = () => {
+  try {
+    let book = data[selectedOnEdit];
+    book.title = inputJudul.value;
+    book.author = inputPenulis.value;
+    book.year = inputTahun.value;
+    book.isComplete = inputStatusBaca.checked;
+
+    data[selectedOnEdit] = book;
+    saveDataToStorage();
+    toggleModalTambahBuku();
+    showNotification({
+      headerMsg: "Berhasil Mengedit!",
+      bodyMsg: "Berhasil Mengedit Informasi Buku",
+    });
+  } catch (err) {
+    showNotification({
+      headerMsg: "Gagal Mengedit!",
+      bodyMsg: err.message,
+    });
+  }
 };
 
 const hapusBuku = (indexBuku) => {
-  let buku = [...data];
-  buku.splice(indexBuku, 1);
-  data = [...buku];
-  saveDataToStorage();
-  showNotification({
-    headerMsg: "Berhasil Menghapus!",
-    bodyMsg: "Berhasil Mengapus 1 Buku!",
-  });
+  try {
+    let buku = [...data];
+    buku.splice(indexBuku, 1);
+    data = [...buku];
+    saveDataToStorage();
+    showNotification({
+      headerMsg: "Berhasil Menghapus!",
+      bodyMsg: "Berhasil Mengapus 1 Buku!",
+    });
+  } catch (err) {
+    showNotification({
+      headerMsg: "Gagal Menghapus!",
+      bodyMsg: err.message,
+    });
+  }
 };
 
 document.addEventListener(RENDER_EVENT, () => {
@@ -202,13 +258,18 @@ document.addEventListener(RENDER_EVENT, () => {
       mainContainer.insertAdjacentHTML("beforeend", template);
     }
 
+    // Register All Button Delete Event Listener in Card Book
     document.querySelectorAll(".button-delete").forEach((item, index) => {
-      item.addEventListener("click", () => {
+      item.addEventListener("click", (ev) => {
+        ev.preventDefault();
         hapusBuku(index);
       });
     });
+
+    // Register All Button Edit Event Listener in Card Book
     document.querySelectorAll(".button-edit").forEach((item, index) => {
-      item.addEventListener("click", () => {
+      item.addEventListener("click", (ev) => {
+        ev.preventDefault();
         tampilkanBuku(index);
       });
     });
@@ -219,32 +280,52 @@ document.addEventListener(RENDER_EVENT, () => {
   }
 });
 
+// Function on Click Event to Filter Data based on Criteria
+filterButton.addEventListener("click", filterBuku);
+
+// Function on Click Event to Reset Criteria Filter to Default
+resetButton.addEventListener("click", () => {
+  clearFilter();
+  getDataFromStorage();
+});
+
+// Add Event Listener to Button "Tambahkan Buku"
+buttonToggleModal.addEventListener("click", () => {
+  modalHeader.innerText = TITLE_MODAL_ADD;
+  buttonSimpan.classList.remove("hidden");
+  buttonEdit.classList.add("hidden");
+  toggleModalTambahBuku();
+});
+
+// Function for close Modal
+closeButtonToggleModal.addEventListener("click", () => {
+  toggleModalTambahBuku();
+});
+
+// Register Button Simpan Listener
+buttonSimpan.addEventListener("click", () => {
+  if (checkingFormValue()) {
+    tambahBuku();
+  } else {
+    showNotification({
+      headerMsg: "Gagal Menambah Data",
+      bodyMsg: "Harap lengkapi Informasi yang dibutuhkan!",
+    });
+  }
+});
+
+// Register Button Edit Listener
+buttonEdit.addEventListener("click", () => {
+  if (checkingFormValue()) {
+    editBuku();
+  } else {
+    showNotification({
+      headerMsg: "Gagal Menambah Data",
+      bodyMsg: "Harap lengkapi Informasi yang dibutuhkan!",
+    });
+  }
+});
+
 document.addEventListener("DOMContentLoaded", () => {
   getDataFromStorage();
-
-  submitForm.addEventListener("submit", (ev) => {
-    ev.preventDefault();
-    tambahBuku();
-
-    setTimeout(() => {
-      showNotification({
-        headerMsg: "Berhasil",
-        bodyMsg: "Berhasil Menambahkan Buku!",
-      });
-    }, 100);
-  });
-
-  buttonToggleModal.addEventListener("click", () => {
-    modalHeader.innerText = TITLE_MODAL_ADD;
-    clearModalTambahBuku();
-    toggleModalTambahBuku();
-  });
-
-  closeButtonToggleModal.addEventListener("click", toggleModalTambahBuku);
-
-  filterButton.addEventListener("click", filterBuku);
-  resetButton.addEventListener("click", () => {
-    clearFilter();
-    getDataFromStorage();
-  });
 });
